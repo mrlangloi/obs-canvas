c6a9723f1f3d292050d84ef65c0138177894eb9e
Rcheu <rcheung1997@gmail.com>
2026-04-09 17:52:44 -0700

    moved CORS origin to variable

--- backend/src/main/java/com/ricky/obscanvas/config/SocketIOConfig.java
+++ backend/src/main/java/com/ricky/obscanvas/config/SocketIOConfig.java
@@ -1,11 +1,16 @@
 package com.ricky.obscanvas.config;
 
 import com.corundumstudio.socketio.SocketIOServer;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SocketIOConfig {
+
+    @Value("${app.frontend.url}")
+    private String frontendUrl;
 
     @Bean
     public SocketIOServer socketIOServer() {
@@ -15,7 +20,7 @@ public class SocketIOConfig {
         config.setPort(5174);
         
         // this allows the frontend to talk to this port
-        config.setOrigin("http://localhost:5173");
+        config.setOrigin(frontendUrl);
 
         // CORS settings to allow the frontend to talk to this port
         config.setAllowHeaders("Content-Type, Authorization, x-requested-with");
