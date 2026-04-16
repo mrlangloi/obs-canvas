ee536b869362ebec3395eb17025c976e44e0c7de
Rcheu <rcheung1997@gmail.com>
2026-04-15 23:45:51 -0700

    removed CardContext context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css'
 import Canvas from './components/Canvas'
-import { CardProvider } from './contexts/CardContext'
 import ControlPanel from './components/ControlPanel'
 import { SocketProvider } from './contexts/SocketContext'
 
@@ -9,15 +8,13 @@ function App() {
     return (
         <>
             <SocketProvider>
-                <CardProvider>
-                    <div className="container">
-                        {/* Control panel for fine-tuning controls */}
-                        <ControlPanel />
+                <div className="container">
+                    {/* Control panel for fine-tuning controls */}
+                    <ControlPanel />
 
-                        {/* Main canvas */}
-                        <Canvas />
-                    </div>
-                </CardProvider>
+                    {/* Main canvas */}
+                    <Canvas />
+                </div>
             </SocketProvider>
         </>
     )
