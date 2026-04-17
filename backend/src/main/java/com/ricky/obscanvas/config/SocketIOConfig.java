1bb48913e6f450b143e7d2fd9bbd9f838e6d4b5a
Rcheu <rcheung1997@gmail.com>
2026-04-17 04:04:12 -0700

    using variables instead of hardcoded values

--- backend/src/main/java/com/ricky/obscanvas/config/SocketIOConfig.java
+++ backend/src/main/java/com/ricky/obscanvas/config/SocketIOConfig.java
@@ -10,17 +10,20 @@ import org.springframework.context.annotation.Configuration;
 public class SocketIOConfig {
 
     @Value("${app.frontend.url}")
-    private String frontendUrl;
+    private String frontendURL;
+
+    @Value("${socket.server.port}")
+    private int socketServerPort;
 
     @Bean
     public SocketIOServer socketIOServer() {
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
         config.setHostname("localhost");
         // where the Socket.io traffic will live
-        config.setPort(5174);
+        config.setPort(socketServerPort);
         
         // this allows the frontend to talk to this port
-        config.setOrigin(frontendUrl);
+        config.setOrigin(frontendURL);
 
         // CORS settings to allow the frontend to talk to this port
         config.setAllowHeaders("Content-Type, Authorization, x-requested-with");
