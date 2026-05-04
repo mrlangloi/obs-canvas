460558705f1b266af857bb89f97c63a7bcacbb56
Rcheu <rcheung1997@gmail.com>
2026-05-03 17:12:48 -0700

    port number moved to .env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  
+  // load env file based on `mode` in the current working directory
+  // the third parameter '' means allow all env variables to be loaded
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [['babel-plugin-react-compiler']],
+        },
+      }),
+    ],
+    server: {
+      port: Number(env.VITE_FRONTEND_PORT) || 3600,
+      strictPort: true,
+    },
+  }
 })
