2ff4c67b0d0c606ffc803a734b867127d4ae2f7c
Rcheu <rcheung1997@gmail.com>
2026-05-11 21:41:01 -0700

    included more attributes

--- frontend/src/types/card.ts
+++ frontend/src/types/card.ts
@@ -5,14 +5,20 @@ interface Position {
 
 export interface CardItem {
     id: string
-    position: Position
     label: string
+    visible: boolean
+
     text: string
     url: string
     mediaType: string
+
+    position: Position
     rotation?: number
     width?: number
     height?: number
+    flipX?: boolean
+    flipY?: boolean
     opacity?: number
+
     zIndex?: number
 }
