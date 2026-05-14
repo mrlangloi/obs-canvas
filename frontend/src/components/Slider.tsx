560d5bd83af1b918bcb44181b806963d87418ded
Rcheu <rcheung1997@gmail.com>
2026-05-13 19:33:15 -0700

    separated label from range slider

--- frontend/src/components/Slider.tsx
+++ frontend/src/components/Slider.tsx
@@ -1,20 +1,29 @@
 import type { SliderItem } from '../types/slider'
 
 const Slider = ({ prop } : { prop: SliderItem }) => {
-    const { name, label, min, max, value, onChange, onMouseUp, onDoubleClick } = prop
+    const { name, label, min, max, value, handleChange, handleMouseUp, handleDoubleClick } = prop
 
     return (
         <div className="slider-container">
-            <label>{label}</label>
+            <div className="slider-label">
+                <label>{label}: </label>
+                <input
+                    className="number-input"
+                    type="number"
+                    value={value}
+                    onChange={handleChange}
+                />
+            </div>
+            {/* <p>{label}: {value}</p> */}
             <input
                 type="range"
                 name={name}
                 min={min}
                 max={max}
                 value={value}
-                onChange={onChange}
-                onMouseUp={onMouseUp}
-                onDoubleClick={onDoubleClick}
+                onChange={handleChange}
+                onMouseUp={handleMouseUp}
+                onDoubleClick={handleDoubleClick}
             />
         </div>
     )
